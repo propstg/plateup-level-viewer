36614d8008d668b681116c5ce72ac232a9fee153
propstg <g@prop.st>
2023-01-23 02:26:00 -0500

    initial commit

---
+++ Main.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using Kitchen;
+using Kitchen.Modules;
+using KitchenLib;
+using KitchenMods;
+using System.Reflection;
+using UnityEngine;
+
+namespace LevelViewer {
+
+    public class LevelViewerMod : BaseMod {
+
+        public const string MOD_ID = "blargle.LevelViewer";
+        public const string MOD_NAME = "Level Viewer";
+        public const string MOD_AUTHOR = "blargle";
+        public const string MOD_VERSION = "0.0.1";
+
+        public LevelViewerMod() : base(MOD_ID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, ">=1.1.3", Assembly.GetExecutingAssembly()) { }
+    }
+
+    public class LevelCollectorSystem : FranchiseFirstFrameSystem, IModSystem {
+
+        public static bool loaded = false;
+        public static SPlayerLevel levelSingleton;
+
+        protected override void OnUpdate() {
+            loaded = true;
+            levelSingleton = GetOrDefault<SPlayerLevel>();
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerElement), "SetPlayer")]
+    class PlayerElement_SetPlayer_Patch {
+
+        public static void Postfix(int id, PlayerElement __instance) {
+            PlayerInfo info = Players.Main.Get(id);
+            if (!info.IsLocalUser || info.IsJoining || !LevelCollectorSystem.loaded) {
+                return;
+            }
+
+            __instance.SetName(info.Profile.Name, $"{info.Username} - Level {LevelCollectorSystem.levelSingleton.Level + 1}");
+            __instance.SetJoinProgress(ProgressionHelpers.GetProgressPercent(LevelCollectorSystem.levelSingleton));
+            var color = info.Profile.Colour;
+            __instance.JoinBar.Color = new Color(color.r, color.g, color.b, 0.5f);
+            __instance.JoinBar.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# PlateUp! Level Viewer
+
+Makes the player info box at the bottom of the screen show your level (by appending to your username) and show your exp progress as a progress bar.
+
+Note: At the moment, this is only visible (and probably updated) when switching profiles (or, if playing with controller, by pressing P briefly to start joining with keyboard.)
+
+![preview](./img/preview.png)
